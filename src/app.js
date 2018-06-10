4bd271ce53b1b0eedf20395e335ca985a73eab14
haneenshahwan4 <haneenshahwan@hotmail.com>
2018-06-10 15:37:32 +0300

    to pull masterter

--- src/app.js
+++ src/app.js
@@ -2,16 +2,10 @@ const express = require('express');
 const exphbs = require('express-handlebars');
 const bodyParser = require('body-parser');
 const controllers = require('./controllers');
-// const helpers = require('handlebars-helpers')();
-
 const helpers = require('./views/helpers/index');
-
-
 const app = express();
 const path = require('path');
 const cookieParser = require('cookie-parser');
-
-
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
@@ -21,7 +15,6 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 app.use(express.static(path.join(__dirname, '..', 'public')));
 app.use(controllers);
-
 app.engine(
 	'hbs',
 	exphbs({
--- src/controllers/dashboard.js
+++ src/controllers/dashboard.js
@@ -1,6 +1,4 @@
 const tabelDashboard = require('./../database/queries/dashboard');
-
-
 exports.get = (req, res) => {
 	tabelDashboard((tabelDashboardError, result) => {
 		if (tabelDashboardError) return res.status(500);
