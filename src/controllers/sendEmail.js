f35b3ac8589ca1b6df34f39f0467f731f5f8ca33
Ahmad <a7m4d.m.sh@gmail.com>
2018-06-24 18:51:08 +0300

    notification

---
+++ src/controllers/sendEmail.js
@@ -0,0 +1,52 @@
+const outBook = require('./../database/queries/outbook');
+const nodemailer = require('nodemailer');
+require('env2')('./config.env');
+const sendEmail=()=>{
+
+	outBook((err,result)=>{
+		if(err) console.log('error',err);
+		else{
+
+			for (var x in result) {
+				const start = result[x].start_date;
+
+				const end = result[x].end_date;
+				let oneDay = 24*60*60*1000;
+
+				let diffDays = Math.round((end.getTime() - start.getTime())/(oneDay));
+
+
+				if(diffDays === 2){
+					const transporter = nodemailer.createTransport({
+						host: 'smtp.gmail.com',
+						port: 587,
+						secure: false,
+						auth: {
+							user:'almanassah@gmail.com',
+							pass: '*20051469'
+						},
+
+					});
+					const mailOptions = {
+						from: '"GSG eman" <contact@gsg.com>',
+						to: result[x].email,
+						subject: 'lending book ',
+						text: 'pleas return book ',
+						html: '<b>Hell world?</b>',
+					};
+
+
+
+
+					transporter.sendMail(mailOptions, (err, info) => {
+						if (err) console.log('err',err);
+						else console.log('send email',info);
+					});
+
+				}
+			}
+		}
+	});
+
+};
+module.exports= sendEmail;
